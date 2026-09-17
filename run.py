28b636d8a7e18ced6aec31707910c3541bf7f809
yanelihernandez <yaneliperdomohernandez@gmail.com>
2026-09-17 16:51:11 +0100

    add node count to run output

--- run.py
+++ run.py
@@ -5,8 +5,13 @@ import search
 ab = search.GPSProblem('A', 'B'
                        , search.romania)
 
-print(search.breadth_first_graph_search(ab).path())
-print(search.depth_first_graph_search(ab).path())
+resultado_bfs, nodos_bfs = search.breadth_first_graph_search(ab)
+print(resultado_bfs.path())
+print("Nodos expandidos (BFS):", nodos_bfs)
+
+resultado_dfs, nodos_dfs = search.depth_first_graph_search(ab)
+print(resultado_dfs.path())
+print("Nodos expandidos (DFS):", nodos_dfs)
 
 # Result:
 # [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
