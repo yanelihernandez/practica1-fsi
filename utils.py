090cacf998eb129a2683e0c8526c026da32d775e
yanelihernandez <yaneliperdomohernandez@gmail.com>
2026-09-17 15:55:28 +0100

    add PriorityQueue

--- utils.py
+++ utils.py
@@ -491,7 +491,7 @@ def DataFile(name, mode='r'):
 
 
 #______________________________________________________________________________
-# Queues: Stack, FIFOQueue
+# Queues: Stack, FIFOQueue, PriorityQueue
 
 class Queue:
     """Queue is an abstract class/interface. There are three types:
@@ -543,7 +543,25 @@ class FIFOQueue(Queue):
             self.start = 0
         return e
 
+    class PriorityQueue(Queue):
+        """Una cola donde el elemento con menor coste sale primero."""
 
+        def __init__(self, coste=lambda x : x):
+            """Inicializamos la cola vacía y el coste de los elementos."""
+            self.items = []
+            self.coste = coste
+
+        def append(self, item):
+            self.items.append(item)
+
+        def __len__(self):
+            return len(self.items)
+
+        def pop(self):
+            """Devuelve el elemento con menor coste y lo elimina de la cola."""
+            minimo = argmin(self.items, self.coste)
+            self.items.remove(minimo)
+            return minimo
 
 ## Fig: The idea is we can define things like Fig[3,10] later.
 ## Alas, it is Fig[3,10] not Fig[3.10], because that would be the same as Fig[3.1]
